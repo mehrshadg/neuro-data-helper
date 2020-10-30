0b5c87daa67705cca6c4544171067ed775f030a5
Mehrshad Golesorkhi <mehrshad_g@outlook.com>
2020-10-30 16:38:10 -0400

    0.0.7

--- neuro_helper/assets/manager.py
+++ neuro_helper/assets/manager.py
@@ -7,4 +7,6 @@ class AssetCategory(Enum):
 
 
 def get_full_path(category: AssetCategory, name: str):
+    if not name:
+        raise ValueError("file name is None.")
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), category.value, name)
--- neuro_helper/create_cortex.py
+++ neuro_helper/create_cortex.py
@@ -9,7 +9,7 @@ names = [
     "template.schaefer2018.2007.32k.dlabel.nii",
     "template.schaefer2018.2007.59k.dlabel.nii",
     "template.wang2015.32k.dlabel.nii",
-    "template.wang2015.59k.dlabel.nii"
+    "template.wang2015.59k.dlabel.nii",
 ]
 for name in names:
     print(name)
@@ -19,4 +19,21 @@ for name in names:
     merged = name.replace(".dlabel.nii", "_cortex.dlabel.nii")
     os.system(f"wb_command -cifti-separate {name} COLUMN -label CORTEX_LEFT {left} -label CORTEX_RIGHT {right}")
     os.system(f"wb_command -cifti-create-label {merged} -left-label {left} -right-label {right}")
+    os.system(f"rm {left} {right}")
+
+
+names = [
+    "topo.t1t2.59k.dscalar.nii",
+    "topo.margulies2016.32k.dscalar.nii",
+    "topo.margulies2016.59k.dscalar.nii",
+
+]
+for name in names:
+    print(name)
+    name = root + name
+    left = name.replace(".dscalar.nii", "_cortexL.func.gii")
+    right = name.replace(".dscalar.nii", "_cortexR.func.gii")
+    merged = name.replace(".dscalar.nii", "_cortex.dscalar.nii")
+    os.system(f"wb_command -cifti-separate {name} COLUMN -metric CORTEX_LEFT {left} -metric CORTEX_RIGHT {right}")
+    os.system(f"wb_command -cifti-create-dense-scalar {merged} -left-metric {left} -right-metric {right}")
     os.system(f"rm {left} {right}")
